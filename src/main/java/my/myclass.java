2200527155306f7a9212d60f4b3a5bf427bde75f
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2024-05-21 20:07:04 +0200

    Update myclass.java

--- src/main/java/my/myclass.java
+++ src/main/java/my/myclass.java
@@ -4,9 +4,16 @@ package my;
 public class myclass {
     public static void main(String[] args) {
         int data=50;
+        System.out.println("Hi from myclass45");  
+         int data=50;
         System.out.println("Hi from myclass45");    
     }
-
+    public static void fun1(String[] args) {
+        int data=50;
+        System.out.println("Hi from myclass45");  
+         int data=50;
+        System.out.println("Hi from myclass45");    
+    }
     //code duplication
             public boolean isSomething()
 {
