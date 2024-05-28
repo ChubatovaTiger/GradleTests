ba5baf042de7956ea8e9ee01bd088a20905edc73
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2024-05-28 09:40:28 +0200

    Update myclass.java

--- src/main/java/my/myclass.java
+++ src/main/java/my/myclass.java
@@ -4,27 +4,6 @@ package my;
 public class myclass {
     public static void main(String[] args) {
         int data=50;
-        System.out.println("Hi from myclass45");  
-         int data=50;
-        System.out.println("Hi from myclass45");    
+        System.out.println("Hi from myclass45");
     }
-    public static void fun1(String[] args) {
-        int data=50;
-        System.out.println("Hi from myclass45");  
-         int data=50;
-        System.out.println("Hi from myclass45");    
-    }
-    //code duplication
-            public boolean isSomething()
-{
-    if (!this.calculateSomeBooleanValue())
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-// end of code duplication
 }
