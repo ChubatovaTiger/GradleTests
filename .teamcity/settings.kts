65eecba4a0b0f6539166f972bac6840b3a288c04
admin <admin@TeamCity>
2024-07-10 16:51:51 +0200

    TeamCity change in 'project1' project: Updated cloud profile 'amazon-1' in project 'project11'

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.amazonEC2CloudImage
 import jetbrains.buildServer.configs.kotlin.amazonEC2CloudProfile
 
 /*
@@ -28,6 +29,20 @@ version = "2024.03"
 project {
 
     features {
+        amazonEC2CloudImage {
+            id = "PROJECT_EXT_4"
+            profileId = "amazon-1"
+            agentPoolId = "1"
+            name = "im1"
+            vpcSubnetId = "subnet-043178c302cabfe37"
+            keyPairName = "chubatova-amazon-ireland"
+            instanceType = "t3.medium"
+            securityGroups = listOf("sg-072d8bfa0626ea2a6")
+            instanceTags = mapOf(
+                "Owner" to "nastasia.chubatova@jetbrains.com"
+            )
+            source = Source("ami-0b55e6eb21b887f36")
+        }
         amazonEC2CloudProfile {
             id = "amazon-1"
             name = "profile1"
