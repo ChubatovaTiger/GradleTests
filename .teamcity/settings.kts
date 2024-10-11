967f627b2bbdd929fcd92ac83d99b6d97651bc4f
admin <admin@TeamCity>
2024-10-11 16:33:51 +0200

    TeamCity change in 'proj1 / proj5' project: project was copied into 'proj1' project

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -26,7 +26,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2024.07"
+version = "2024.03"
 
 project {
 
@@ -60,6 +60,7 @@ project {
         }
     }
 
+    subProject(Proj6)
     subProject(Proj5)
     subProject(Proj4)
 }
@@ -132,6 +133,66 @@ object Proj5_Build2 : BuildType({
 })
 
 object Proj5_Build3 : BuildType({
+    name = "build3"
+
+    artifactRules = "a*.txt"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            id = "simpleRunner"
+            scriptContent = "echo a > a%system.teamcity.buildType.id%.%build.number%.txt"
+        }
+    }
+})
+
+
+object Proj6 : Project({
+    name = "proj6"
+
+    buildType(Proj6_Build3)
+    buildType(Proj6_Build1)
+    buildType(Proj6_Build2)
+})
+
+object Proj6_Build1 : BuildType({
+    name = "build1"
+
+    artifactRules = "a*.txt"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            id = "simpleRunner"
+            scriptContent = "echo a > a%system.teamcity.buildType.id%.%build.number%.txt"
+        }
+    }
+})
+
+object Proj6_Build2 : BuildType({
+    name = "build2"
+
+    artifactRules = "a*.txt"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            id = "simpleRunner"
+            scriptContent = "echo a > a%system.teamcity.buildType.id%.%build.number%.txt"
+        }
+    }
+})
+
+object Proj6_Build3 : BuildType({
     name = "build3"
 
     artifactRules = "a*.txt"
