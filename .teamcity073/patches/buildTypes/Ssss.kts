b01b967313be56ecd4bc8252f4db323ac63f1cf7
admin <admin@TeamCity>
2024-11-04 21:19:41 +0100

    TeamCity change in 'project1' project: 'ssss' build configuration was paused

--- .teamcity073/patches/buildTypes/Ssss.kts
+++ .teamcity073/patches/buildTypes/Ssss.kts
@@ -10,6 +10,11 @@ To apply the patch, change the buildType with id = 'Ssss'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Ssss")) {
+    check(paused == false) {
+        "Unexpected paused: '$paused'"
+    }
+    paused = true
+
     expectSteps {
     }
     steps {
