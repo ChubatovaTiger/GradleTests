515d636e584dd59ec4be0ea9fdeb30cc1ffadf64
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2025-03-04 17:01:20 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,70 +1,48 @@
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
-version = "2024.12"
+version = "2021.2"
 
 project {
 
-    buildType(Sdf)
-    buildType(Build)
+    vcsRoot(HttpsGithubComGradleGradleGit)
+
+
+    val numProjects=2
+    val numConfigurationsPerProject=40
+
+
+    for (i in 0..numProjects) {
+        subProject {
+            id("subProj_$i")
+            name = "subProj $i"
+
+            for (j in 0..numConfigurationsPerProject) {
+                buildType {
+                    id("subProj_bt_$i" + "_$j")
+                    name = "bt $i $j"
+                    
+    vcs {
+        root(HttpsGithubComGradleGradleGit)
+    }
+
+                }
+            }
+        }
+    }
 }
 
-object Build : BuildType({
-    name = "Build"
 
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        script {
-            id = "simpleRunner"
-            scriptContent = "hj"
-            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
-            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
-            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {
-        perfmon {
-        }
-    }
+object HttpsGithubComGradleGradleGit : GitVcsRoot({
+    name = "https://github.com/ChubatovaTiger/ChubatovaGradleTestsBackup"
+    url = "https://github.com/ChubatovaTiger/ChubatovaGradleTestsBackup"
+    branch = "refs/heads/master"
 })
 
-object Sdf : BuildType({
-    name = "pdf"
 
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-})
+
+
