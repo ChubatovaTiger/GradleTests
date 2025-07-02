2fe3b8b801e0389f137b3226f2180bee2fdb5560
admin <admin@TeamCity>
2025-07-02 11:02:26 +0200

    runners of 'build1' build configuration were updated (TeamCity change in 'project1' project)

--- .teamcity/Project1/buildTypes/Project1_Build1.kt
+++ .teamcity/Project1/buildTypes/Project1_Build1.kt
@@ -2,17 +2,12 @@ package Project1.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.ant
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object Project1_Build1 : BuildType({
     uuid = "9ef5aa98-9ff7-4dcf-8759-8c8b3fce64e7"
     name = "build1"
 
     steps {
-        script {
-            id = "simpleRunner"
-            scriptContent = "echo a"
-        }
         ant {
             id = "Ant"
             mode = antFile {
