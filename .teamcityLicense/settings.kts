fb5e35e84e283282caeb658faf80103b0cc7cb6e
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2025-09-05 15:23:07 +0200

    Update settings.kts

--- .teamcityLicense/settings.kts
+++ .teamcityLicense/settings.kts
@@ -1,67 +1,35 @@
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
-version = "2025.03"
+version = "2021.2"
 
 project {
 
-    buildType(Build1)
-    buildType(Build2)
+
+
+    val numProjects=200
+    val numConfigurationsPerProject=2
+
+
+    for (i in 0..numProjects) {
+        subProject {
+            id("subProj_$i")
+            name = "subProj $i"
+
+            for (j in 0..numConfigurationsPerProject) {
+                buildType {
+                    id("subProj_bt_$i" + "_$j")
+                    name = "bt $i $j"
+                     params {
+                param("a", "val1")
+                    }
+
+                }
+            }
+        }
+    }
 }
-
-object Build1 : BuildType({
-    name = "build1"
-
-    vcs {
-        root(DslContext.settingsRoot, "+:folder => folder")
-
-        checkoutDir = "folder/folder"
-    }
-
-    steps {
-        script {
-            id = "simpleRunner"
-            scriptContent = "ls"
-        }
-    }
-})
-
-object Build2 : BuildType({
-    name = "build2"
-
-    vcs {
-        root(DslContext.settingsRoot, "+:folder2 => folder2")
-
-        checkoutDir = "folder/folder2"
-    }
-
-    steps {
-        script {
-            id = "simpleRunner"
-            workingDir = "../folder"
-            scriptContent = "ls"
-        }
-    }
-})
