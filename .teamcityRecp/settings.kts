a9d82dc5fcc465cf2a6765002f6166a8b93654ce
admin <admin@TeamCity>
2025-09-10 17:25:12 +0200

    runners of 'RecipeChange' build configuration were updated (TeamCity change in 'recipe' project)

--- .teamcityRecp/settings.kts
+++ .teamcityRecp/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -38,5 +39,12 @@ object RecipeChange : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            id = "simpleRunner"
+            scriptContent = "echo %a%"
+        }
     }
 })
