b3148f0f2ddca95d7a57c7a2bd50c7cde8090b2e
admin <admin@TeamCity>
2025-11-05 14:05:33 +0100

    build features of 'both_tmpl' build configuration were updated (TeamCity change in 'project1Dsl' project)

--- .teamcityArtDistDSL/_Self/buildTypes/BothTmpl.kt
+++ .teamcityArtDistDSL/_Self/buildTypes/BothTmpl.kt
@@ -1,8 +1,16 @@
 package _Self.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.parallelTests
 
 object BothTmpl : BuildType({
     templates(Tmpl)
     name = "both_tmpl"
+
+    features {
+        parallelTests {
+            id = "BUILD_EXT_1"
+            numberOfBatches = 10
+        }
+    }
 })
