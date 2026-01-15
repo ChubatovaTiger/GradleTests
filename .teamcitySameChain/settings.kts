e4d5e280b2df11248236e201e8058bc8076974f6
admin <admin@TeamCity>
2026-01-15 08:40:23 +0100

    Versioned settings configuration updated (TeamCity change in 'project2' project)

---
+++ .teamcitySameChain/settings.kts
@@ -0,0 +1,148 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2025.11"
+
+project {
+
+    buildType(Build3)
+    buildType(Build2)
+    buildType(Build1)
+    buildType(Build4)
+}
+
+object Build1 : BuildType({
+    name = "Build1"
+
+    artifactRules = "a*"
+
+    vcs {
+        root(DslContext.settingsRoot, "+:src => .")
+    }
+
+    steps {
+        script {
+            id = "simpleRunner"
+            scriptContent = "echo %build.number% > a-%build.number%.txt"
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+})
+
+object Build2 : BuildType({
+    name = "Build2"
+
+    artifactRules = "a*"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            id = "simpleRunner"
+            scriptContent = "ls | grep a"
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+
+    dependencies {
+        artifacts(Build1) {
+            artifactRules = "a*"
+        }
+    }
+})
+
+object Build3 : BuildType({
+    name = "Build3"
+
+    artifactRules = "a*"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            id = "simpleRunner"
+            scriptContent = "ls | grep a"
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+
+    dependencies {
+        dependency(Build1) {
+            snapshot {
+                reuseBuilds = ReuseBuilds.NO
+            }
+
+            artifacts {
+                artifactRules = "a*"
+            }
+        }
+    }
+})
+
+object Build4 : BuildType({
+    name = "Build4"
+
+    artifactRules = "a*"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            id = "simpleRunner"
+            scriptContent = "ls | grep a"
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+
+    dependencies {
+        artifacts(Build1) {
+            buildRule = sameChain()
+            artifactRules = "a*"
+        }
+    }
+})
